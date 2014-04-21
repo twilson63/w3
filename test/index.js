f6c82d12ed30a40c767018c534339bc58c5b54f9
Tom Wilson <tom@jackhq.com>
2014-04-20 21:40:07 -0400

    modified test port

--- test/index.js
+++ test/index.js
@@ -2,31 +2,19 @@ var server = require('../'),
   specify = require('specify');
   request = require('request');
 
-server(3000, 'test');
+server(4000, 'test');
 
 specify('test root no slash', function(assert) {
-  request('http://localhost:3000', function(e,r,b) {
+  request('http://localhost:4000', function(e,r,b) {
     assert.ok(/Hello World/.test(b));
   });
 });
 
 specify('test specific page', function(assert) {
-  request.get('http://localhost:3000/fixture.html', function(e,r,b) {
+  request.get('http://localhost:4000/fixture.html', function(e,r,b) {
     assert.equal('<h1>Hello World</h1>\n', b);
   });
 });
 
 specify.run();
 
-// Added test to ignore query strings...
-/*
-request('http://localhost:3000/test/fixture.html?fooo=barr', function(e, r, b){
-  assert.equal(b,'<h1>Hello World</h1>\n');
-  //assert.equal('<h1>Hello</h1>', b);
-  process.exit(0);
-});
-  
-request('http://localhost:3000', function(e,r,b) {
-  assert.equal(b, '<h1>Main Page</h1>');
-});
-*/
